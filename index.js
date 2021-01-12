da4f31622dcbb2ba70ab0bbc78387973c24bd96a
Hoang Tran <son.hoang01@gmail.com>
2021-01-12 22:11:05 +0800

    download json file

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ program
   .command("download")
   .alias("d")
   .option("-f, --format <format_type>", "format type: JSON")
+  .option("--path <path>", "path that you want to put json file.")
   .option(
     "-p, --profile <profile_name>",
     "profile name that you want to download"
@@ -28,6 +29,8 @@ program
   .action(async function () {
     const _format = this.format ? this.format : "JSON";
     const _profile = this.profile ? this.profile : "ALL";
+    const _path = this.path ? this.path : "";
+    console.log('_path => ', _path);
     const _rc = loadRcFile("i18n");
     const _config = _rc
       .split(" ")
@@ -38,6 +41,11 @@ program
       }, {});
 
     let _request_url = `${API_URL}/profile?token=${_config.token}`;
+    
+    const convertJsonI18n = arr => {
+      let object  = (obj, item) => Object.assign(obj, { [item.key]: item.value })
+      return arr.reduce(object, {});
+    }
 
     if (_profile != "ALL") {
       _request_url += `$lang=${_profile}`;
@@ -51,13 +59,16 @@ program
     spinner.succeed();
 
     //  write json file
-    const file = `${__dirname}/test/data.json`;
-    const _json = { name: 'JP' }
+    res.data.forEach( profile => {
+      const file = `${__dirname}${_path}/${profile.name}.json`;
+      console.log('file => ', file);
+      const _json = convertJsonI18n(profile.i18n);
+      
+      jsonfile.writeFile(file, _json,{ spaces: 2 }, function (err) {
+        if (err) console.error(err)
+      });
+    });
     
-    jsonfile.writeFile(file, _json,{ spaces: 2 }, function (err) {
-      if (err) console.error(err)
-    })
-    console.log(res.data);
   });
 
 function loadRcFile(rcFileName) {
@@ -76,71 +87,4 @@ function loadRcFile(rcFileName) {
   }
 }
 
-function formatJSON(json,textarea) {
-  var nl;
-  if(textarea) {
-      nl = "&#13;&#10;";
-  } else {
-      nl = "<br>";
-  }
-  var tab = "&#160;&#160;&#160;&#160;";
-  var ret = "";
-  var numquotes = 0;
-  var betweenquotes = false;
-  var firstquote = false;
-  for (var i = 0; i < json.length; i++) {
-      var c = json[i];
-      if(c == '"') {
-          numquotes ++;
-          if((numquotes + 2) % 2 == 1) {
-              betweenquotes = true;
-          } else {
-              betweenquotes = false;
-          }
-          if((numquotes + 3) % 4 == 0) {
-              firstquote = true;
-          } else {
-              firstquote = false;
-          }
-      }
-
-      if(c == '[' && !betweenquotes) {
-          ret += c;
-          ret += nl;
-          continue;
-      }
-      if(c == '{' && !betweenquotes) {
-          ret += tab;
-          ret += c;
-          ret += nl;
-          continue;
-      }
-      if(c == '"' && firstquote) {
-          ret += tab + tab;
-          ret += c;
-          continue;
-      } else if (c == '"' && !firstquote) {
-          ret += c;
-          continue;
-      }
-      if(c == ',' && !betweenquotes) {
-          ret += c;
-          ret += nl;
-          continue;
-      }
-      if(c == '}' && !betweenquotes) {
-          ret += nl;
-          ret += tab;
-          ret += c;
-          continue;
-      }
-      if(c == ']' && !betweenquotes) {
-          ret += nl;
-          ret += c;
-          continue;
-      }
-      ret += c;
-  } // i loop
-  return ret;
-}
 program.parse(process.argv);
